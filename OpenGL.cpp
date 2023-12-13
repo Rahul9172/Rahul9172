f5ee5f3ef9d868ab311f89a7f0e9976b1549c2d1
Rahul9172 <152916610+Rahul9172@users.noreply.github.com>
2023-12-13 10:47:51 +0530

    Add files via upload

---
+++ OpenGL.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<GL/glut.h>
+void init(void)
+{
+glClearColor(1.0,1.0,1.0,0.0);
+glMatrixMode(GL_PROJECTION);
+gluOrtho2D(0.0,800.0,0.0,600.0);
+}
+void createline(void)
+{
+glClear(GL_COLOR_BUFFER_BIT);
+glColor3f(0.0,1.0,0.0);
+glBegin(GL_LINES);
+ glVertex2i(200,100);
+ glVertex2i(20,50);
+glEnd();
+glFlush();
+}
+int main(int argc,char**argv)
+{
+glutInit(& argc,argv);
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+glutInitWindowPosition(0,0);
+glutInitWindowSize(800,600);
+glutCreateWindow("The OpenGL example");
+init();
+glutDisplayFunc(createline);
+glutMainLoop();
+}
